90daf5c0c96230f3c1199355104e06a91174adab
letiencao <caole28072000@gmail.com>
2020-04-28 15:16:31 +0700

    first commit

---
+++ src/main/java/com/springmvc/controller/admin/HomeController.java
@@ -0,0 +1,16 @@
+package com.springmvc.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller(value = "homeControllerOfAdmin")
+public class HomeController {
+
+	@RequestMapping(value = "/quan-tri/trang-chu", method = RequestMethod.GET)
+	public ModelAndView homePage() {
+		ModelAndView mav = new ModelAndView("admin/home");
+		return mav;
+	}
+}
---
+++ src/main/java/com/springmvc/controller/web/HomeController.java
@@ -0,0 +1,16 @@
+package com.springmvc.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller(value = "homeControllerOfWeb")
+public class HomeController {
+
+	@RequestMapping(value = "/trang-chu", method = RequestMethod.GET)
+	public ModelAndView homePage() {
+		ModelAndView mav = new ModelAndView("web/home");
+		return mav;
+	}
+}
